b0d0185c380665f7c54203556f4a55ec002a1ca1
Artem <ab-grishin@mail.ru>
2019-11-14 20:28:31 +0300

    2.4.5

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,108 +1,60 @@
 package com.example.myapplication;
 
+
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
-import android.widget.Button;
-import android.widget.GridLayout;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
-import task.Sub;
-import task.Task;
-
-public class MainActivity extends Activity implements OnClickListener,
-        OnLongClickListener {
-
-    private int WIDTH = 10;
-    private int HEIGHT = 10;
-
-    private Button[][] cells;
-
+public class MainActivity extends Activity {
+    // Вызывается при создании активности
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cells);
-        makeCells();
-
-        generate();
-
+        // Инициализирует активность.
+        setContentView(R.layout.activity_main);
     }
 
-    void generate() {
-
-        //Эту строку нужно удалить
-
-
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                if (Math.random() >= 0.5) {
-                    cells[i][j].setBackgroundColor(Color.YELLOW);
+    /** Вызывается при нажатии пользователем на кнопку Решить */
+    public void solveEquation(View view) {
+        // ax+b=c
+        double a = Double.parseDouble( ((EditText)
+                findViewById(R.id.coefficient_a)).getText().toString());
+        double b = Double.parseDouble( ((EditText)
+                findViewById(R.id.coefficient_b)).getText().toString());
+        double c = Double.parseDouble( ((EditText)
+                findViewById(R.id.coefficient_c)).getText().toString());
+        TextView result = (TextView) findViewById(R.id.result);
+        if (a==0){
+            if (b==0){
+                if (c==0) {
+                    result.setText(""+"Уравнение имеет бесконечно много решений");
+                }else{
+                    result.setText(""+"Уравнение не имеет решений");
+                }
+            }else{
+                if (c==0){
+                    result.setText(""+"x="+0);
+                }else{
+                    result.setText(""+"x="+String.valueOf(-c/b));
+                }
+            }
+        }else{
+            double D=b*b-4*a*c;
+            if (D>0){
+                result.setText(""+"x1="+String.valueOf((-b+Math.sqrt(D))/2*a)+"\nx2="+String.valueOf((-b-Math.sqrt(D))/2*a));
+            }else{
+                if (D==0){
+                    result.setText(""+"x="+String.valueOf((-b)/2*a));
+                }else{
+                    result.setText(""+"Уравнение не имеет решений");
                 }
             }
         }
     }
-
-    @Override
-    public boolean onLongClick(View v) {
-        //Эту строку нужно удалить
-        return false;
-    }
-
-    @Override
-    public void onClick(View v) {
-        //Эту строку нужно удалить
-
-        Button tappedCell = (Button) v;
-
-        //Получаем координтаты нажатой клетки
-        int tappedX = getX(tappedCell);
-        int tappedY = getY(tappedCell);
-
-        for (int x = 0; x < WIDTH; x++)
-        {
-            cells[tappedY][x].setBackgroundColor(Color.RED);
-        }
-        for (int y = 0; y < WIDTH; y++)
-        {
-            cells[y][tappedX].setBackgroundColor(Color.RED);
-        }
-
-    }
-
-    /*
-     * NOT FOR THE BEGINNERS
-     * ==================================================
-     */
-
-    int getX(View v) {
-        return Integer.parseInt(((String) v.getTag()).split(",")[1]);
-    }
-
-    int getY(View v) {
-        return Integer.parseInt(((String) v.getTag()).split(",")[0]);
-    }
-
-    void makeCells() {
-        cells = new Button[HEIGHT][WIDTH];
-        GridLayout cellsLayout = (GridLayout) findViewById(R.id.CellsLayout);
-        cellsLayout.removeAllViews();
-        cellsLayout.setColumnCount(HEIGHT);
-        for (int i = 0; i < HEIGHT; i++)
-            for (int j = 0; j < WIDTH; j++) {
-                LayoutInflater inflater = (LayoutInflater) getApplicationContext()
-                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                cells[i][j] = (Button) inflater.inflate(R.layout.cell, cellsLayout, false);
-                cells[i][j].setOnClickListener(this);
-                cells[i][j].setOnLongClickListener(this);
-                cells[i][j].setTag(i + "," + j);
-                cellsLayout.addView(cells[i][j]);
-            }
-    }
-
 }
